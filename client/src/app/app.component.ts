967f5f87832d8a06781d803df6e3ce5fd2e1b847
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-01-26 17:39:12 +0530

    new changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { StudentService } from './student.service';
 import { studentRegistration } from './student';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'client';
+  title = 'Yug';
   student: studentRegistration = {
     email: "student1@gmail.com",
     password: "student1",
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { StudentLoginComponent } from './student-login/student-login.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: AppComponent },
+    { path: "studentLogin", component: StudentLoginComponent }
+];
