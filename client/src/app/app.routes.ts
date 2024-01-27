e0bff0d4681301c726da19a46de2ecbaf88eacbb
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2024-01-27 21:31:14 +0530

    added student registration

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { StudentLoginComponent } from './student-login/student-login.component';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { StudentRegistrationComponent } from './student-registration/student-registration.component';
 
 export const routes: Routes = [
     { path: "studentLogin", component: StudentLoginComponent },
-    { path: 'landing-page', component: LandingPageComponent }
+    { path: 'landing-page', component: LandingPageComponent },
+    { path: "studentRegistration", component: StudentRegistrationComponent }
 ];
